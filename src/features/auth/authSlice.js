695c6fb4d8b3bc062100728f657e462008ecc60e
Eva Garcia <evagarciaclemente96@gmail.com>
2024-06-03 23:47:29 +0200

    authSlice.js

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  user: null,
+  token: null
+}
+
+export const authSlice = createSlice({
+  name:"auth",
+  initialState,
+  reducers:{}
+})
+
+export default authSlice.reducer
